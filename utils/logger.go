e75746543e43f762f9ec86cc2ca765264cad9c18
John Langs <johnslangsiii@gmail.com>
2023-06-14 01:59:22 -0500

    Implemented logging for PUT and DELETE

--- utils/logger.go
+++ utils/logger.go
@@ -1,118 +1,11 @@
-package main
+package utils
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
 	"os"
-	"sync"
-
-	"github.com/gorilla/mux"
+	"text/scanner"
 )
-
-var store = struct {
-	sync.RWMutex
-	m map[string]string
-}{m: make(map[string]string)}
-
-func Put(key string, value string) error {
-	store.Lock()
-	store.m[key] = value
-	store.Unlock()
-
-	println(len(store.m))
-
-	return nil
-}
-
-var ErrorNoSuchKey = errors.New("no such key")
-
-func Get(key string) (string, error) {
-	store.Lock()
-	value, ok := store.m[key]
-	store.Unlock()
-
-	if !ok {
-		return "", ErrorNoSuchKey
-	}
-
-	println(len(store.m))
-
-	return value, nil
-}
-
-func Delete(key string) error {
-	store.Lock()
-	delete(store.m, key)
-	store.Unlock()
-
-	println(len(store.m))
-
-	return nil
-}
-
-func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["key"]
-
-	value, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
-
-	if err != nil {
-		http.Error(w,
-			err.Error(),
-			http.StatusInternalServerError)
-		return
-	}
-
-	err = Put(key, string(value))
-	if err != nil {
-		http.Error(w,
-			err.Error(),
-			http.StatusInternalServerError)
-		return
-	}
-
-	logger.WritePut(key, string(value))
-	w.WriteHeader(http.StatusCreated)
-}
-
-func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["key"]
-
-	err := Delete(key)
-	if err != nil {
-		http.Error(w,
-			err.Error(),
-			http.StatusInternalServerError)
-		return
-	}
-
-	logger.WriteDelete(key)
-	// TODO: Modify for alternate response for deleting a non-existant key
-	w.WriteHeader(http.StatusOK)
-}
-
-func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["key"]
-
-	value, err := Get(key)
-	if errors.Is(err, ErrorNoSuchKey) {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write([]byte(value))
-}
 
 type TransactionLogger interface {
 	WriteDelete(key string)
@@ -259,23 +152,4 @@ func initializeTransactionLog() error {
 	logger.Run()
 
 	return err
-}
-
-
-func helloMuxHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello gorilla/mux\n"))
-}
-
-func main() {
-	r := mux.NewRouter()
-	initializeTransactionLog()
-
-	r.HandleFunc("/", helloMuxHandler)
-
-	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
-	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
-	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
-
-	println("Service Started. Listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
 }
