e249748667fb0bb75509f13d15aea618b0964d35
John Langs <johnslangsiii@gmail.com>
2023-06-13 22:21:24 -0500

    Added additional test handlers

--- main.go
+++ main.go
@@ -11,10 +11,26 @@ func helloMuxHandler(w http.ResponseWriter, r* http.Request) {
 	w.Write([]byte("Hello gorilla/mux\n"))
 }
 
+func ProductHandler(w http.ResponseWriter, r* http.Request) {
+	vars := mux.Vars(r)
+	w.Write([]byte(vars["key"]))
+}
+
+func ArticlesCategoryHandler(w http.ResponseWriter, r* http.Request) {
+
+}
+
+func ArticleHandler(w http.ResponseWriter, r* http.Request) {
+
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", helloMuxHandler)
+	r.HandleFunc("/products/{key}", ProductHandler)
+	r.HandleFunc("/articles/{category}/", ArticlesCategoryHandler)
+	r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
 
 	println("Service Started. Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
