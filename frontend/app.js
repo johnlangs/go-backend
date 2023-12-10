46526fdb1bea9c7a003d862fde881e0bef065725
John Langs <me@johnlangs.net>
2023-12-09 20:15:57 -0600

    added updated frontend

---
+++ frontend/app.js
@@ -0,0 +1,44 @@
+const postKeyField = document.getElementById('post-key')
+const postValueField = document.getElementById('post-value')
+const postOutput = document.getElementById('output-post')
+const postButton = document.getElementById('post-key-value-button')
+const postForm = document.getElementById('post-form')
+
+const getKeyField = document.getElementById('get-key')
+const getOutput = document.getElementById('output')
+const getForm = document.getElementById('get-form')
+
+async function postKeyValue() {
+    let key =  postKeyField.value;
+    let value = postValueField.value;
+    let url = "https://go-api.johnlangs.net/v1/" + key;
+
+    console.log(key)
+    console.log(value)
+    console.log(url)
+
+    await fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: value
+    });
+
+    postOutput.textContent = "Posted!";
+}
+
+async function getKey() {
+    let key =  postKeyField.value;
+    url = "https://go-api.johnlangs.net/v1/" + key;
+
+    console.log(key)
+    console.log(url)
+
+    let res = await fetch(url, {
+        method: "GET",
+    });
+    res = await res.text();
+
+    getOutput.textContent = res;
+}
